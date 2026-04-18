fbaddcef01ddf36999d7d186e55e51b944e9960e
hoaho9068 <hoaho9068@gmail.com>
2026-04-18 12:59:57 -0500

    added borders and random wall generation
    feature: walls and borders were added to map generation
    added player: used as a test to run the game with the script player controller (basic movement)

--- Assets/Scripts/Managers/MapGenerator.cs
+++ Assets/Scripts/Managers/MapGenerator.cs
@@ -2,15 +2,49 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
+    public GameObject wallPrefab;
+
+    public int width = 50;
+    public int height = 50;
+    public int wallCount = 50;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("MapGenerator running");
+        GenerateMap();
+        CreateBorderWalls();
     }
 
-    // Update is called once per frame
-    void Update()
+    void GenerateMap()
     {
-        
+        //space between tiles
+        float tileSize = 1f;
+
+        for (int x = -width / 2; x < width / 2; x++)
+        {
+            for (int y = -height / 2; y < height / 2; y++)
+            {
+                Vector3 position = new Vector3(x * tileSize, y * tileSize, 0);
+                //random walls
+                if (Random.value < 0.2f)
+                {
+                    Instantiate(wallPrefab, position, Quaternion.identity);
+                }
+            }
+        }   
+    }
+    void CreateBorderWalls()
+    {
+        for (int x = -width / 2; x <= width / 2; x++)
+        {
+            Instantiate(wallPrefab, new Vector3(x, -height / 2, 0), Quaternion.identity); //bottom border
+            Instantiate(wallPrefab, new Vector3(x, height / 2, 0), Quaternion.identity); // top border
+        }
+        for (int y = -height / 2; y <= height / 2; y++)
+        {
+            Instantiate(wallPrefab, new Vector3(-width / 2, y, 0), Quaternion.identity); // left border
+            Instantiate(wallPrefab, new Vector3(width / 2, y, 0), Quaternion.identity); //right border
+        }
     }
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 1f;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(horizontal, vertical);
+        rb.linearVelocity = movement * speed;
+    }
+}
